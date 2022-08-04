d8258132844c70642b5e1f14f78fee9036ce4f57
praweb <prasanna@Prasannas-MBP.lan>
2022-08-04 22:54:40 +0100

    add source for netlify

--- web/next.config.js
+++ web/next.config.js
@@ -4,7 +4,29 @@ const nextConfig = {
   swcMinify: true,
   images: {
 		domains: ['cdn.sanity.io']
-	}
+	},
+  // Make sure that your node enviroment supports async/await
+  exportPathMap: async function (defaultPathMap) {
+  
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "toy"].slug.current')
+      .then(data =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+            ...acc,
+            [`/${slug}`]: { page: '/${slug}', query: { slug } }
+          }),
+          {}
+        )
+      )
+      .catch(console.error)
+    return path
+  }
 }
 
 module.exports = nextConfig
