71146c19addd56a84b420c92b122808a01e6f8b0
praweb <prasanna@Prasannas-MBP.lan>
2022-08-05 10:23:29 +0100

    add preview url

--- web/next.config.js
+++ web/next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
   },
   // Make sure that your node enviroment supports async/await
   exportPathMap: async function (defaultPathMap) {
-  
+    const secret = 'j8heapkqy4rdz6kudrvsc7ywpvfhrv022abyx5zgmuwpc1xv'
     const path = await client
       // get all the posts and return those with slugs
       .fetch('*[_type == "toy"].slug.current')
@@ -22,15 +22,16 @@ const nextConfig = {
         // use reduce to build an object with routes
         // and select the blog.js file, and send in the
         // correct query paramater.
-        data.reduce(
+        const routes = data.reduce(
           (acc, slug) => ({
             '/': { page: '/' },
             ...acc,
-            [`/${slug}`]: { page: "/[slug]", query: { slug } }
+            [`/${slug}`]: { page: "/[slug]", query: { slug } },
+            '/api/preview': { page: '/api/preview', query: {secret, slug}}
           }),
           {}
         )
-        return data;
+        console.log(routes)
       })
       .catch(console.error)
     return path
