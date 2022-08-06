0e419e1b28e7af8985a2c0feffb915c3e667a665
praweb <prasanna@Prasannas-MacBook-Pro.local>
2022-08-06 20:22:08 +0100

    add imgix loader

--- web/next.config.js
+++ web/next.config.js
@@ -3,13 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-		domains: ['cdn.sanity.io']
+		domains: ['cdn.sanity.io'],
+    loader: 'imgix'
 	},
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  }
+  // experimental: {
+  //   images: {
+  //     unoptimized: true,
+  //   },
+  // }
 }
 
 module.exports = nextConfig
